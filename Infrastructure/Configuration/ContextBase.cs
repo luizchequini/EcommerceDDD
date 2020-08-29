b804c4c9f0b26b4410a10f13fd16c9b892c988d1
Luiz Chequini <luizchequini@hotmail.com>
2020-08-29 11:19:29 -0300

    Aula 4 - Implementando projeto Infra

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options):base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConnectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        private string GetStringConnectionConfig()
+        {
+            string strCon = "Server=LAPTOP-CV9KCCO0\\SQLEXPRESS;Database=EcommerceDDD;Trusted_Connection=True;MultipleActiveResultSets=true";
+            //string strCon = "Server=apolo.hostsrv.org,1433;Database=EcommerceDDD;User ID=cyberlacs777;Password=N75l%5sg";
+            return strCon;
+        }
+    }
+}
